1215c4adea7c0146775be5d505e9b3283b92e0cf
Sam-Roggeman <sam.roggeman@student.uantwerpen.be>
2020-11-11 21:31:17 +0100

    LinkedChainTable.py methods

--- LinkedChainTable.py
+++ LinkedChainTable.py
@@ -1,4 +1,46 @@
 
 
 class LinkedChainTable():
-    def __init__(self):pass
+    def __init__(self):
+        pass
+
+
+    def tableIsEmpty(self):     #{query}
+        """
+
+        :return: bool
+        """
+        pass
+
+
+    def tableLength(self):      #{query}
+        """
+
+        :return: int
+        """
+        pass
+
+
+    def tableDelete(self, searchKey):
+        """
+
+        :param searchKey:
+        :return:
+        """
+        pass
+
+    def tableRetrieve(self, searchKey):
+        """
+
+        :param searchKey:
+        :return boolean:
+        """
+        pass
+
+
+
+    def traverseTable(self):
+        """
+
+        """
+        pass
