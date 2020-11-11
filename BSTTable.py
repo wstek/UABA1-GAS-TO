fdb466ec80b2012e3a83d5c4b2e5a7b70475c9c3
SaidYandarbiev <Said.Yandarbiev@student.uantwerpen.be>
2020-11-11 21:25:45 +0100

    HIER

--- BSTTable.py
+++ BSTTable.py
@@ -1,4 +1,23 @@
 
 
 class BSTTable():
-    def __init__(self):pass
+    def __init__(self):
+        """
+
+        """
+        pass
+
+    def tableInsert(self,item):
+        pass
+
+    def tableDelete(self,Searchkey):
+        pass
+
+    def tableRetrieve(self,Searchkey):
+        pass
+
+    def traverseTable(self,visit):
+        pass
+
+    def tableLength(self):
+        pass
