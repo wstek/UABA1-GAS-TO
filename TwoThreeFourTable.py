238d38619092529cdaacc8b3cc12511a51f4ae05
SaidYandarbiev <72447539+SaidYandarbiev@users.noreply.github.com>
2020-12-20 22:42:20 +0100

    Add files via upload

---
+++ TwoThreeFourTable.py
@@ -0,0 +1,17 @@
+class TwoThreeFourTreeTable:
+    def __init__(self):
+        self.tabel = TwoThreeFourTree()
+    def tableIsEmpty(self):
+        return self.tabel.isEmpty()
+    def tableInsert(self,item):
+        return self.tabel.insertItem(item)
+    def tableRetrieve(self,key):
+        return self.tabel.retrieveItem(key)
+    def traverseTable(self,Functie):
+        return self.tabel.inorderTraverse(self,Functie)
+    def save(self):
+        return self.tabel.save()
+    def tableDelete(self,key):
+        return self.tabel.deleteItem(key)
+    def load(self,dict):
+        return self.tabel.load(dict)
